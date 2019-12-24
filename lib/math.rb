2cd74f29bd44baa159d99a083a1a6c4982fb1f2c
nickie kane <kameo712@gmail.com>
2019-12-24 21:19:32 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  return sum
 end
 
 def subtraction(num1, num2)
-
+  dif = num2 - num1
+  return dif 
 end
 
 def division(num1, num2)
-
+  div = num2 / num1
+  return div
 end
 
 def multiplication(num1, num2)
-
+  mul = num1 * num2
+  return mul
 end
 
 def modulo(num1, num2)
-
+  mod = num2 % num1
+  return mod
 end
 
 def square_root(num)
-
+  squ = 
+  return squ
 end
